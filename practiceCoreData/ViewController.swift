99f5d86149a28fbac3cca18631cfcb66f3db75c1
Kadeem Best <kadeem@zebras-MacBook-Pro.local>
2020-05-21 11:53:47 -0400

    Initial Commit

---
+++ practiceCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  practiceCoreData
+//
+//  Created by Kadeem Best on 2020-05-21.
+//  Copyright © 2020 ios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
